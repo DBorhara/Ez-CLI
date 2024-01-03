b3360933dfea0796a19e2cc3dc4c4cc7c91e758a
Depak Borhara <depakborhara@gmail.com>
2024-01-03 15:18:36 -0500

    Test Commit

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use std::env;
+use std::env::{self, args};
 use std::error::Error;
 use std::io::{self, Write};
 use std::process::Command;
@@ -6,19 +6,14 @@ use std::process::Command;
 const GIT: &str = "git";
 
 fn main() -> Result<(), Box<dyn Error>> {
-    let args: Vec<String> = env::args().collect();
-
-    let remote = args.get(1).unwrap_or(&"origin".to_string()).clone();
-    let branch = args.get(2).unwrap_or(&current_branch()?).clone();
-
-    run_git_commands(&remote, &branch)?;
+    run_git_commands()?;
     Ok(())
 }
 
-fn run_git_commands(remote: &str, branch: &str) -> Result<(), Box<dyn Error>> {
+fn run_git_commands() -> Result<(), Box<dyn Error>> {
     git_command(&["add", "-A"])?;
     git_command(&["commit", "-m", &message_input()?])?;
-    git_command(&["push", remote, branch])?;
+    git_command(&["push", &remote_input()?, &branch_input()?])?;
 
     println!("Successfully added, committed, and pushed all changes.");
     Ok(())
@@ -49,6 +44,36 @@ fn message_input() -> Result<String, Box<dyn Error>> {
     }
 }
 
+fn remote_input() -> Result<String, Box<dyn Error>> {
+    let args: Vec<String> = env::args().collect();
+
+    if args.len() > 1 {
+        Ok(args[1].clone())
+    } else {
+        print!("Enter remote name: ");
+        io::stdout().flush()?;
+
+        let mut remote = String::new();
+        io::stdin().read_line(&mut remote)?;
+        Ok(remote.trim().to_string())
+    }
+}
+
+fn branch_input() -> Result<String, Box<dyn Error>> {
+    let args: Vec<String> = env::args().collect();
+
+    if args.len() > 1 {
+        Ok(args[1].clone())
+    } else {
+        print!("Enter branch name: ");
+        io::stdout().flush()?;
+
+        let mut branch = String::new();
+        io::stdin().read_line(&mut branch)?;
+        Ok(branch.trim().to_string())
+    }
+}
+
 fn current_branch() -> Result<String, Box<dyn Error>> {
     let output = Command::new(GIT)
         .args(&["rev-parse", "--abbrev-ref", "HEAD"])
@@ -60,4 +85,3 @@ fn current_branch() -> Result<String, Box<dyn Error>> {
         Ok(String::from_utf8(output.stdout)?.trim().to_string())
     }
 }
-
